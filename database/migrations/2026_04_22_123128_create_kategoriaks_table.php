3bb0069df635d112bc5d7f5f97264f9ac3ddde67
Nádas Viktória Mária (SZF_2024_2026) <nadas.viktoria.maria@diak.szamalk-szalezi.hu>
2026-04-22 14:45:47 +0200

    táblák oszlop neveinek beállítása

--- database/migrations/2026_04_22_123128_create_kategoriaks_table.php
+++ database/migrations/2026_04_22_123128_create_kategoriaks_table.php
@@ -6,20 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+    
     public function up(): void
     {
         Schema::create('kategoriaks', function (Blueprint $table) {
             $table->id();
+            $table->string("katnev");
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+    
     public function down(): void
     {
         Schema::dropIfExists('kategoriaks');
--- database/migrations/2026_04_22_123201_create_tevekenysegeks_table.php
+++ database/migrations/2026_04_22_123201_create_tevekenysegeks_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('tevekenysegeks', function (Blueprint $table) {
             $table->id();
+            $table->foreignId("kat_id");
+            $table->string("tev_nev");
+            $table->string("allapot");
             $table->timestamps();
         });
     }
