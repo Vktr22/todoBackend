17c887afcd5219f3b674997ab8382740f882fcc7
Nádas Viktória Mária (SZF_2024_2026) <nadas.viktoria.maria@diak.szamalk-szalezi.hu>
2026-04-22 14:32:48 +0200

    projekt backend létrehozása

---
+++ app/Http/Controllers/TevekenysegekController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\tevekenysegek;
+use App\Http\Requests\StoretevekenysegekRequest;
+use App\Http\Requests\UpdatetevekenysegekRequest;
+
+class TevekenysegekController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoretevekenysegekRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(tevekenysegek $tevekenysegek)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(tevekenysegek $tevekenysegek)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatetevekenysegekRequest $request, tevekenysegek $tevekenysegek)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(tevekenysegek $tevekenysegek)
+    {
+        //
+    }
+}
---
+++ database/factories/TevekenysegekFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\tevekenysegek;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<tevekenysegek>
+ */
+class TevekenysegekFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
